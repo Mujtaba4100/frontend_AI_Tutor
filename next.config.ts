10459836ec1e5ed13bbba1f32c5f5f299ccc0ba7
Mujtaba4100 <saad444hassan@gmail.com>
2026-03-08 07:03:06 +0500

    issueb resolve

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8001',
+  },
 };
 
 export default nextConfig;
